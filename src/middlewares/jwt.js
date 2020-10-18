9039eecb5812415aae3a25704509bf9b784547e4
mjbkhorasani <node.js.developers.kh@gmail.com>
2020-10-18 11:40:26 +0330

    jwt middleware

--- src/middlewares/jwt.js
+++ src/middlewares/jwt.js
@@ -1,21 +1,26 @@
 const jwt = require('../utils/jwt');
 const User = require('../models/user')
 const Role = require('../models/role');
+const ErrorResponse = require('../utils/error-response');
 
-module.exports.isAuth = async (req, res, next) => {
+module.exports.isUser = async (req, res, next) => {
     let authHeader = req.headers.authorization;
 
     if (authHeader) {
         let token = authHeader.split(' ')[1];
         let decoded = await jwt.verifyToken(token);
-
-        // fetch user info: 
-        let user = await User.findByPk(decoded.id);
-        // check user privilages:
-        let role = await Role.findByPk();
-        console.log(decoded.id);
-        // next();
+        let user = await User.findByPk(decoded.sub);
+        
+        if (user) {
+            let role = await Role.findByPk(user.roleId);
+            // console.log('Role: %s', user);
+            console.log('Role: %s', role);
+            role.accessLevel === 4 ? next() : next(new ErrorResponse('Unauthorized', 'You are not authorized.', 401));
+        } else {
+            console.log('User: %s', user);
+            next(new ErrorResponse('Unauthorized', 'You are not authorized.', 401));
+        };
     } else {
-        req
+        next(new ErrorResponse('jwtError', 'Something went wrong in the user JWT middleware', 401));
     };
 };
