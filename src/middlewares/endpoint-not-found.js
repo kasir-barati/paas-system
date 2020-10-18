67e2c17ba88aa8f3434afe56c1562b82cfbd5f80
mjbkhorasani <node.js.developers.kh@gmail.com>
2020-10-18 09:55:02 +0330

    404 error handled successfully

--- src/middlewares/endpoint-not-found.js
+++ src/middlewares/endpoint-not-found.js
@@ -3,16 +3,18 @@ const Logger = require('../utils/logger');
 const logger = new Logger('page-not-found');
 
 module.exports = (req, res, next) => {
-    logger.warn('PageNotFound', {
-        meta: {
-            "ip": req.ip,
-            "method": req.method,
-            "originalUrl": req.originalUrl,
-            "userAgent": req.get('user-agent'),
-        }
-    });
-    req.apiStatus = 404;
-    req.apiData = null;
-    req.apiError = "Page not found";
+    if (!req.apiStatus) {
+        logger.warn('PageNotFound', {
+            meta: {
+                "ip": req.ip,
+                "method": req.method,
+                "originalUrl": req.originalUrl,
+                "userAgent": req.get('user-agent'),
+            }
+        });
+        req.apiStatus = 404;
+        req.apiData = null;
+        req.apiError = "Page not found";
+    };
     next();
 };
